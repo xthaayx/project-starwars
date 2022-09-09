e99f0e1e9fb382b0e8798b29fdf37ae34fb78021
Thaysa <thay.fernnanda@gmail.com>
2022-09-09 18:32:00 -0300

    Requisito 5

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,25 +1,45 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import testData from '../../cypress/mocks/testData';
+import useEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import AppPlanetsProvider from '../context/AppPlanetsProvider';
+import userEvent from '@testing-library/user-event';
 
-// test('I am your test', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/Hello, App!/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
 
 describe('Teste de input', () => {
+  global.fetch = () => (
+    Promise.resolve({
+      json: () => (
+        Promise.resolve(testData)
+      ),
+    })
+  )
+
   it('testa input', () => {
-      render(<App />);
+    act(()=> {render(<AppPlanetsProvider><App /></AppPlanetsProvider>)})
 
-      const nameImput = screen.getByTestId("name-filter")
-      const columnInput = screen.getByTestId("column-filter")
-      const comparisonInput = screen.getByTestId("comparison-filter")
-      const valueInput = screen.getByTestId("value-filter")
+      const table = screen.getByRole("table");
+      const nameImput = screen.getByTestId("name-filter");
+      const columnInput = screen.getByTestId("column-filter");
+      const comparisonInput = screen.getByTestId("comparison-filter");
+      const valueInput = screen.getByTestId("value-filter");
+      const button = screen.getByRole("button", {
+        name:/filtrar/i
+      });
 
+      expect(table).toBeInTheDocument();
       expect(nameImput).toBeInTheDocument();
       expect(columnInput).toBeInTheDocument();
       expect(comparisonInput).toBeInTheDocument();
       expect(valueInput).toBeInTheDocument();
+      userEvent.click(button);
+      expect(button).toBeInTheDocument();
+  });
+
+  it('testa filter', async () => {
+    act(()=> {render(<AppPlanetsProvider><App /></AppPlanetsProvider>)}) 
+    expect(screen.getByTestId('value-filter')).toBeInTheDocument()
   });
 })
